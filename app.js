8d8babe7f62cde7ad1b812ae3f867831d1620103
Arslan <ali.arslan319@gmail.com>
2021-08-24 22:44:13 -0400

    create game and UI element variables

---
+++ app.js
@@ -0,0 +1,12 @@
+let min = 1
+let max = 10
+let winningNum = 2
+let guessesLeft = 3
+
+// UI elements 
+const game = document.querySelector('#game')
+const minNum = document.querySelector('.min-num')
+const maxNum = document.querySelector('.max-num')
+const guessButton = document.querySelector('#guess-button')
+const guessInput = document.querySelector('#guess-input')
+const guessButton = document.querySelector('.message')
