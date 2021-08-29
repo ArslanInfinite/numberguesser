2d91328ee6460aabd2303c38d0175522378c4c52
Arslan <ali.arslan319@gmail.com>
2021-08-28 20:26:43 -0400

    create guessButton event listener to check for user input

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ const minNum = document.querySelector('.min-num')
 const maxNum = document.querySelector('.max-num')
 const guessButton = document.querySelector('#guess-button')
 const guessInput = document.querySelector('#guess-input')
-const guessButton = document.querySelector('.message')
+const message = document.querySelector('.message') 
+
+// assigning UI min and max
+// what is line 16 and 17 doing?
+minNum.textContent = min 
+maxNum.textContent = max
+
+// listening for guess input 
+
+guessButton.addEventListener('click', function(){
+    console.log(guessInput.value)
+})
